4d80b09bd3240310fa7ad370c7931d830589ab13
Andrés <nosoysaenz@gmail.com>
2021-06-26 10:30:36 -0600

    sonidos

--- Assets/Scripts/Organ.cs
+++ Assets/Scripts/Organ.cs
@@ -13,22 +13,27 @@ public class Organ : MonoBehaviour {
     public float deterRate = 0.01f;
 
     [HideInInspector] public float Health { get; set; }
+    [HideInInspector] public bool Death { get; set; }
 
     private void Start() {
         image.sprite = sprites[0];
         Health = maxHealth;
+        Death = false;
     }
 
     private void Update() {
-        Health -= deterRate;
-        if(Health < maxHealth / 4) {
-            image.sprite = sprites[3];
-        } else if (Health < 2 * maxHealth / 4) {
-            image.sprite = sprites[2];
-        } else if (Health < 3 * maxHealth / 4) {
-            image.sprite = sprites[1];
-        } else {
-            image.sprite = sprites[0];
-        }
+        if (!Death) {
+            Health -= deterRate;
+            if (Health <= 0) {
+                image.sprite = sprites[3];
+                Death = true;
+            } else if (Health < maxHealth / 3) {
+                image.sprite = sprites[2];
+            } else if (Health < 2 * maxHealth / 3) {
+                image.sprite = sprites[1];
+            } else {
+                image.sprite = sprites[0];
+            }
+        }    
     }
 }
