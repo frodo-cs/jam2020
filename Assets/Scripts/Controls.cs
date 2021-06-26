e3a5935de96651aee3c882ca761dc4128a7a1088
Thiff12 <thifag12@gmail.com>
2021-06-26 00:45:05 -0600

    sliderController

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Controls : MonoBehaviour
 {
-
+    public Slider bloodLeft;
+    public Slider bloodToGive;
     int n;
     public void OnButtonPress()
     {
         n++;
         Debug.Log("Button clicked " + n + " times.");
     }
+
+
  
 }
---
+++ Assets/Scripts/SliderController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour
+{
+    public Text sliderValue;
+    public Slider slider;
+    int value = 0;
+
+    // Start is called before the first frame update
+    public void OnSliderChanged(float value)
+    {
+        sliderValue.text = slider.value.ToString();
+    }
+
+    public void UpdateBlood()
+    {
+        value++;
+        slider.value = value;
+    }
+}
