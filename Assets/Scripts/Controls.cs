873f388b2cdb40590cea0bb858a1b8166ddfde5e
Thiff12 <thifag12@gmail.com>
2021-06-25 23:33:05 -0600

    buttons

---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+
+    int n;
+    public void OnButtonPress()
+    {
+        n++;
+        Debug.Log("Button clicked " + n + " times.");
+    }
+ 
+}
