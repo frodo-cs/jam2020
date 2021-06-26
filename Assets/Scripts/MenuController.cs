1e31e90e873f6e177c450ce4154f318069d273b8
YerGuerrero <40374686+YerGuerrero@users.noreply.github.com>
2021-06-25 23:06:14 -0600

    Add scripts

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuController : MonoBehaviour
+{
+    public string scene;
+    public string instructions;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("space"))
+        {
+            SceneManager.LoadScene(scene);
+        }
+
+        else if (Input.GetKey("i"))
+        {
+            SceneManager.LoadScene(instructions);
+        }
+    }
+
+}
