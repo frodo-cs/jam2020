89f4705abbdd61803e5e8520a4e59b59158aac16
Andrés <nosoysaenz@gmail.com>
2021-06-26 08:51:10 -0600

    Timer

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+
+    [SerializeField] TextMeshProUGUI textTime;
+    [SerializeField] int maxTime;
+    float time;
+
+    private void Start() {
+        time = maxTime;
+        textTime.text = $"{maxTime}";
+    }
+
+    private void Update() {
+        if(time > 0) {
+            time -= Time.deltaTime;
+            textTime.text = time.ToString("0");
+        } else {
+            textTime.text = "0";
+            GameEvents.current.GameWonTrigger();
+        }
+    }
+
+}
