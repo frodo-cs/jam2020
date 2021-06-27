d232d25a6cbce463209dcd99063a1c173eb01100
Thiff12 <thifag12@gmail.com>
2021-06-27 01:12:59 -0600

    Blood To Give

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -3,26 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Controls : MonoBehaviour {
+public class Controls : MonoBehaviour
+{
     [SerializeField] AudioSource source;
-    public Slider bloodCount;
-    public Slider bloodToGive;
-    int n;
+    [SerializeField] Slider bloodCount;
+    [SerializeField] Slider bloodToGive;
     
-    public void OnButtonPress() {
-        n++;
-        Debug.Log("Button clicked " + n + " times.");
+    public void OnButtonPress(string name)
+    {
+        
+        bloodCount.value -= bloodToGive.value;
+        float sum = Organs.organs[name].Health + bloodToGive.value;
+        if (sum > Organs.organs[name].maxHealth)
+        {
+            Organs.organs[name].Health = Organs.organs[name].maxHealth;
+        }
+        else
+        {
+            Organs.organs[name].Health = sum;
+        }
     }
 
     public void GeneralButton()
     {
         source.Play();
 
-    }
-
-    private void FixedUpdate()
-    {
-        
     }
 
 }
